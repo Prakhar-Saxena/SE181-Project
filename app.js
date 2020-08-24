53945a4f49eb7422139adb309ca037d9e9873c7f
Luke Midock <lukemidock@144.121.140.150.lightower.net>
2020-08-24 11:14:55 -0400

    front end updates

--- app.js
+++ app.js
@@ -22,9 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/chess', chessRouter);
+app.use('/', chessRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
